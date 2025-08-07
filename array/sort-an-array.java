9eb644b6f1ecd311c0191bf912f3c1f0e955ec78
revanthm1902 <revanthm051@gmail.com>
2025-08-07 13:35:27 +0530

    LC-sort-an-array [Runtime: 25 ms; Memory: 57.3 MB]

--- array/sort-an-array.java
+++ array/sort-an-array.java
@@ -1,37 +1,46 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+        merge(nums,0,nums.length-1);
         return nums;
     }
-
-    private void quickSort(int[] A, int lo, int hi) {
-        if (lo >= hi) return;
-
-        int mid = lo + (hi - lo) / 2;
-        int pivot = median(A[lo], A[mid], A[hi]);
-
-        if (pivot == A[lo] && lo != hi) swap(A, lo, hi);
-        else if (pivot == A[mid] && mid != hi) swap(A, mid, hi);
-
-        int i = lo, lt = lo, gt = hi;
-        while (i <= gt) {
-            if (A[i] < pivot) swap(A, lt++, i++);
-            else if (A[i] > pivot) swap(A, i, gt--);
-            else i++;
+    public static void merge(int[] arr,int stIdx,int endIdx)
+    {
+        if(stIdx>=endIdx) return;
+        int mid=(stIdx+endIdx)/2;
+        merge(arr,stIdx,mid);
+        merge(arr,mid+1,endIdx);
+        mergeSort(arr,stIdx,mid,endIdx);
+    }
+    public static void mergeSort(int[] arr,int left,int mid,int high)
+    {
+        int[] temp=new int[high-left+1];
+        int right=mid+1,idx=0;
+        int start=left;
+        while(left<=mid&&right<=high)
+        {
+            if(arr[left]>arr[right])
+            {
+                temp[idx++]=arr[right];
+                right++;
+            }
+            else
+            {
+                temp[idx++]=arr[left];
+                left++;
+            }
         }
-
-        quickSort(A, lo, lt - 1);
-        quickSort(A, gt + 1, hi);
-    }
-
-    private int median(int a, int b, int c) {
-        if ((a >= b && a <= c) || (a <= b && a >= c)) return a;
-        if ((b >= a && b <= c) || (b <= a && b >= c)) return b;
-        return c;
-    }
-
-    private void swap(int[] A, int i, int j) {
-        if (i == j) return;
-        int t = A[i]; A[i] = A[j]; A[j] = t;
+        while(left<=mid)
+        {
+            temp[idx++]=arr[left];
+            left++;
+        }
+        while(right<=high)
+        {
+            temp[idx++]=arr[right];
+            right++;
+        }
+        for (int m=0;m<temp.length;m++) {
+            arr[start+m]=temp[m];
+        }
     }
 }
