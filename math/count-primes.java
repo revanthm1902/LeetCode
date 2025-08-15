893d63c20a4e8a3d10fd8ddd9f3b2e1ef0a58af4
revanthm1902 <revanthm051@gmail.com>
2025-08-16 03:25:41 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

---
+++ math/count-primes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+        boolean[] isP= new boolean[n+1];
+        Arrays.fill(isP,true);
+
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(isP[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    isP[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<=n;i++){
+            if(isP[i]) count++;
+        }
+        return count;
+    }
+}
