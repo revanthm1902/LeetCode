83a245194f2383700e60a86645f36d8dbf01a437
Revanth Modalavalasa <revanthm051@gmail.com>
2025-10-17 23:08:13 +0530

    LC-max-consecutive-ones [Runtime: 2 ms; Memory: 50.2 MB]

--- array/max-consecutive-ones.java
+++ array/max-consecutive-ones.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int count=0;
-        int max=0;
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             count= (nums[i]==1) ? count+1 : 0;
             if(count>max) max=count;
