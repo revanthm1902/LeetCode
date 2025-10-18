4bf0ec512ff579035f52c1ae64f230fad8c03d72
Revanth Modalavalasa <revanthm051@gmail.com>
2025-10-18 19:21:01 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- hash-table/subarray-sum-equals-k.java
+++ hash-table/subarray-sum-equals-k.java
@@ -1,22 +1,19 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int l=0,r=0;
-        int sum=nums[0];
-        int len=0;
-        int n=nums.length;
+        int l = 0, r = 0;
+        int sum = 0;
+        int count = 0;
+        int n = nums.length;
 
-        while(r<n){
-            sum+=nums[r];
-            while(l<=r && sum>k){
-                sum-=nums[l];
+        while (r < n) {
+            sum += nums[r];
+            while (l <= r && sum > k) {
+                sum -= nums[l];
                 l++;
             }
-            if(sum==k){
-                len++;
-                l++;
-            }
+            if (sum == k) count++;
             r++;
         }
-        return len;
+        return count;
     }
 }
