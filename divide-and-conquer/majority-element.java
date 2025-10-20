6bcd9ad38a86a2055eb3de4fb0a6c9bd142dab47
Revanth Modalavalasa <revanthm051@gmail.com>
2025-10-20 22:55:34 +0530

    LC-majority-element [Runtime: 2 ms; Memory: 53.4 MB]

---
+++ divide-and-conquer/majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int candidate = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+        if (count > nums.length / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+}
