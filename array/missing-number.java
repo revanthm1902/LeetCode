84808a6ea8ca27a3e7ae0db5eb706095a2e03594
Revanth Modalavalasa <revanthm051@gmail.com>
2025-10-29 15:41:38 +0530

    LC-missing-number [Runtime: 0 ms; Memory: 45.5 MB]

--- array/missing-number.java
+++ array/missing-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int expectedSum = n * (n + 1) / 2;
-        int actualSum = 0;
-        for (int num : nums) actualSum += num;
-        return expectedSum - actualSum;
+        int expSum = n * (n + 1) / 2;
+        int actSum = 0;
+        for (int num : nums) actSum += num;
+        return expSum - actSum;
     }
 }
