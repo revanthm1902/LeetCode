70bf47d09e27784f5ac18f2a450f099f37364210
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-08 06:32:52 +0530

    LC-sort-an-array [Runtime: N/A; Memory: N/A]

--- bucket-sort/sort-an-array.java
+++ bucket-sort/sort-an-array.java
@@ -1,30 +1,37 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        sort(nums,0,nums.length-1);
+        quickSort(nums, 0, nums.length - 1);
         return nums;
     }
-    public int[] sort(int[] nums, int l, int h) {
-        if(l<h){
-            int pi=partition(nums,l,h);
-            sort(nums,l,pi-1);
-            sort(nums,pi+1,h);
+
+    public void quickSort(int[] nums, int l, int h) {
+        if (l < h) {
+            int pi = partition(nums, l, h);
+            quickSort(nums, l, pi - 1);
+            quickSort(nums, pi + 1, h);
         }
-        return nums;
     }
-    public int partition(int[] nums,int l, int h){
-        int pi=nums[l],i=l+1,j=h;
-        while(i<=j){
-            while(i<h && nums[i]<pi) i++;
-            while(j>l && nums[j]>pi) j--;
-            if(i<j) swap(nums,i,j);
+
+    public int partition(int[] nums, int l, int h) {
+        int pi = nums[l];  // pivot
+        int i = l + 1;
+        int j = h;
+
+        while (i <= j) {
+            while (i <= h && nums[i] <= pi) i++;
+            while (nums[j] > pi) j--;
+
+            if (i < j) swap(nums, i, j);
             else break;
         }
-        swap(nums,j,l);
+
+        swap(nums, l, j);
         return j;
     }
-    public void swap(int[] nums,int i,int j){
-        int t=nums[i];
-        nums[i]=nums[j];
-        nums[j]=t;
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
