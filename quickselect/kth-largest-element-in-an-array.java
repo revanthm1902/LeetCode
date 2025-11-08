a3726a5856e90b637ff44d5708717a4ecc4eba38
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-08 08:09:08 +0530

    LC-kth-largest-element-in-an-array [Runtime: N/A; Memory: N/A]

--- quickselect/kth-largest-element-in-an-array.java
+++ quickselect/kth-largest-element-in-an-array.java
@@ -2,36 +2,11 @@ import java.util.*;
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int left = 0, right = nums.length - 1;
-        int targetIndex = nums.length - k; // kth largest = (n - k)th smalles
-        while (left <= right) {
-            int pivotIndex = partition(nums, left, right);
-            if (pivotIndex == targetIndex) {
-                return nums[pivotIndex];
-            } else if (pivotIndex < targetIndex) {
-                left = pivotIndex + 1;
-            } else {
-                right = pivotIndex - 1;
-            }
+        int n=nums.length-1;
+        Arrays.sort(nums);
+        for(int i=n;i>=0;i--){
+            return nums[n-k]+1;
         }
-        return -1; // should never happen
-    }
-    public static int partition(int[] nums, int low, int high) {
-        int pi = nums[low];
-        int i = low+1;
-        int j = high;
-        while (i <= j) {
-            while (i <= high && nums[i] <= pi) i++;
-            while (nums[j] > pi) j--;
-            if (i < j) swap(nums, i, j);
-            else break;
-        }
-        swap(nums, j, low);
-        return j;
-    }
-    public static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        return -1;
     }
 }
