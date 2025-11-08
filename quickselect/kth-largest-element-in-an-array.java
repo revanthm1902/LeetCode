1abaa25d538b7b1831a5a6057699a7920f275b7d
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-08 08:09:50 +0530

    LC-kth-largest-element-in-an-array [Runtime: N/A; Memory: N/A]

--- quickselect/kth-largest-element-in-an-array.java
+++ quickselect/kth-largest-element-in-an-array.java
@@ -4,7 +4,7 @@ class Solution {
     public int findKthLargest(int[] nums, int k) {
         int n=nums.length-1;
         Arrays.sort(nums);
-        for(int i=n;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             return nums[n-k]+1;
         }
         return -1;
