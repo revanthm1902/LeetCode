37fa7b5966255a22d06f674a4c7b653349e4b975
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-09 22:39:42 +0530

    LC-count-negative-numbers-in-a-sorted-matrix [Runtime: 1 ms; Memory: 46.9 MB]

--- binary-search/count-negative-numbers-in-a-sorted-matrix.java
+++ binary-search/count-negative-numbers-in-a-sorted-matrix.java
@@ -2,7 +2,7 @@ class Solution {
     public int countNegatives(int[][] grid) {
         int neg=0;
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid.length;j++){
+            for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j]<0) neg++;
             }
         }
