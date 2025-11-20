906325a87fb3ff49606973e98807689d020b5d85
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-20 17:52:50 +0530

    LC-unique-paths [Runtime: 0 ms; Memory: 41.9 MB]

--- math/unique-paths.java
+++ math/unique-paths.java
@@ -1,6 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        if(m==1 || n==1) return 1;
-        return uniquePaths(m-1,n)+uniquePaths(m,n-1);
+        int[] dp=new int[n];
+        dp[0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[j]=dp[j]+dp[j-1];
+            }
+        }
+        return dp[n-1];
     }
 }
