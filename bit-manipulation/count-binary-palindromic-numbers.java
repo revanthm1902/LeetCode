2b36266deece5655b0674c6a98157436082825ea
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-21 04:08:11 +0530

    LC-count-binary-palindromic-numbers [Runtime: N/A; Memory: N/A]

--- bit-manipulation/count-binary-palindromic-numbers.java
+++ bit-manipulation/count-binary-palindromic-numbers.java
@@ -1,25 +1,13 @@
 class Solution {
     public int countBinaryPalindromes(long n) {
+
         int count = 0;
         for (long i = 0; i <= n; i++) {
-            if (isPal(i)) {
+            String bin = Long.toBinaryString(i);
+            if (bin.equals(new StringBuilder(bin).reverse().toString())) {
                 count++;
             }
         }
         return count;
-    }
-
-    public boolean isPal(long x) {
-        String s1 = "";
-        String s2 = "";
-        long temp = x;
-        if (temp == 0) return true;
-        while (temp > 0) {
-            long bit = temp % 2;
-            s1 = s1 + bit; 
-            s2 = bit + s2;
-            temp /= 2;
-        }
-        return s1.equals(s2);
     }
 }
