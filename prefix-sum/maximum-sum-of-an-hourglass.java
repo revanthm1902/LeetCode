c95f2a382179f77a45f82dee36857e01d144068c
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-21 06:22:19 +0530

    LC-maximum-sum-of-an-hourglass [Runtime: 8 ms; Memory: 47.2 MB]

---
+++ prefix-sum/maximum-sum-of-an-hourglass.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        int r=grid.length;
+        int c=grid[0].length;
+        int max=Integer.MIN_VALUE;
+        if(r<3 || c<3) return -1;
+        for(int i=0;i<r-2;i++){
+            for(int j=0;j<c-2;j++){
+                int sum=grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
+                max=Math.max(sum,max);
+            }
+        }
+        return max;
+    }
+}
