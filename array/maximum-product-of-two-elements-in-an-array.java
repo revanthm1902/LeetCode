8e58c1a7f9f643cd377d6a059dff17ada70a5157
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-22 21:24:47 +0530

    LC-maximum-product-of-two-elements-in-an-array [Runtime: 0 ms; Memory: 44.4 MB]

--- array/maximum-product-of-two-elements-in-an-array.java
+++ array/maximum-product-of-two-elements-in-an-array.java
@@ -1,7 +1,14 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n=nums.length;
-        return (nums[n-1]-1)*(nums[n-2]-1);
+        int max1 = 0, max2 = 0;
+        for (int x : nums) {
+            if (x >= max1) {
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max2 = x;
+            }
+        }
+        return (max1 - 1) * (max2 - 1);
     }
 }
