7d61015d5e6d4d8043fc36fe42f2b95d556a782b
Revanth Modalavalasa <revanthm051@gmail.com>
2025-11-22 23:22:28 +0530

    LC-find-greatest-common-divisor-of-array [Runtime: 0 ms; Memory: 45.1 MB]

--- number-theory/find-greatest-common-divisor-of-array.java
+++ number-theory/find-greatest-common-divisor-of-array.java
@@ -1,11 +1,13 @@
 class Solution {
     public int findGCD(int[] nums) {
-        Arrays.sort(nums);
         int n=nums.length;
-        int min=nums[0];
-        int max=nums[n-1];
-        int res=gcd(min,max);
-        return res;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+            if(num>max) max=num;
+            if(num<min) min=num;
+        }
+        return gcd(min,max);
     }
     static int gcd(int a,int b){
         if(a==0) return b;
