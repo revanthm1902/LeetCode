d8c521863266c3ccd33bd3627b92cbee5aa0d95c
Revanth Modalavalasa <revanthm051@gmail.com>
2025-12-17 08:49:11 +0530

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/longest-substring-without-repeating-characters.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        int max=Integer.MIN_VALUE;
+        int i=0; int j=0;
+
+        Set<Character> set=new HashSet<>();
+        while(j<n){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+                max=Math.max(max,j-i);
+            } else{
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return max;
+    }
+}
