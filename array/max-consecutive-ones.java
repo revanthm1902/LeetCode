36c3ddde8d4de2a59270a8145d6a2707f4d73771
Revanth Modalavalasa <revanthm051@gmail.com>
2026-01-03 20:49:31 +0530

    Synchronize existing submission history (1/2)

--- array/max-consecutive-ones.java
+++ array/max-consecutive-ones.java
@@ -3,9 +3,10 @@ class Solution {
         int count=0;
         int max=0;
         for(int i=0;i<nums.length;i++){
-            count= (nums[i]==1) ? count+1 : 0;
+            count = (nums[i]==1) ? count+1 : 0;
             if(count>max) max=count;
         }
+        
         return max;
     }
 }
