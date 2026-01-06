a8bb6ca3bbb36eec593d40f0d9fe42918ba6a5ec
Revanth Modalavalasa <revanthm051@gmail.com>
2026-01-06 22:24:56 +0530

    LC-palindrome-number [Runtime: 6 ms; Memory: 46 MB]

--- math/palindrome-number.java
+++ math/palindrome-number.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int d=x;
-        int rn=0;
-        while(x>0){
-            int ld=x%10;
-            if(x>Integer.MAX_VALUE/10 && ld>7){
+        String s = String.valueOf(x);
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
                 return false;
             }
-            
-            rn=(rn*10)+ld;
-            x=x/10;
+            l++;
+            r--;
         }
-        if(d==rn){
-            return true;
-        }else{
-            return false;
-        }
+        return true;
     }
 }
