5b3d35cb650823637c4ecbecc4273918afa142aa
Revanth Modalavalasa <revanthm051@gmail.com>
2026-03-14 22:35:32 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.java
+++ math/reverse-integer.java
@@ -1,17 +1,11 @@
 class Solution {
     public int reverse(int x) {
-        int revn =0;
+        int rn=0;
         while(x!=0){
-            int ld= x % 10;
-            if (revn > Integer.MAX_VALUE / 10 || (revn == Integer.MAX_VALUE / 10 && ld > 7)) {
-                return 0;
-            }
-            if (revn < Integer.MIN_VALUE / 10 || (revn == Integer.MIN_VALUE / 10 && ld < -8)) {
-                return 0;
-            }
-            revn=(revn*10)+ld;
-            x=x/10;
+            int ld=x%10;
+            rn=(rn*10)+ld;
+            x/=10;
         }
-        return revn;
+        return rn;
     }
 }
