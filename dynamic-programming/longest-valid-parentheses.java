0980004b3388b76f4efc9f59d3a3484a5833c355
Revanth Modalavalasa <revanthm051@gmail.com>
2026-04-02 15:50:37 +0530

    LC-longest-valid-parentheses [Runtime: 5 ms; Memory: 46.3 MB]

---
+++ dynamic-programming/longest-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack <Integer> st=new Stack<>();
+        st.push(-1);
+        int max=0;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                st.push(i);
+            }else{
+                st.pop();
+
+                if(st.isEmpty()){
+                    st.push(i);
+                }else{
+                    int l=i-st.peek();
+                    max=Math.max(max,l);
+                }
+            }
+        }
+        return max;
+    }
+}
