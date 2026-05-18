1fd776387a60a64f042f17b3175ae5e7ab548dd0
Revanth Modalavalasa <revanthm051@gmail.com>
2026-05-18 16:22:15 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,3 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s)==Counter(t)
+        if(len(s)!=len(t)):
+            return False
+        mp={}
+        for ch in s:
+            mp[ch]=mp.get(ch,0)+1
+        for ch in t:
+            if ch not in mp:
+                return False
+            mp[ch]-=1
+            if(mp[ch]<0):
+                return False
+        return True
