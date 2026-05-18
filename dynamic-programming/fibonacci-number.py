8dcbd45b36c84d661d5208d87157b8c4cea144e7
Revanth Modalavalasa <revanthm051@gmail.com>
2026-05-18 16:48:32 +0530

    LC-fibonacci-number [Runtime: N/A; Memory: N/A]

--- dynamic-programming/fibonacci-number.py
+++ dynamic-programming/fibonacci-number.py
@@ -2,4 +2,11 @@ class Solution:
     def fib(self, n: int) -> int:
         if n<=1:
             return n
-        return self.fib(n-1)+self.fib(n-2)
+        prev2=0
+        prev1=1
+
+        for i in range(2,n+1):
+            curr=prev1+prev2
+            prev2=prev1
+            prev1=curr
+        return prev1
