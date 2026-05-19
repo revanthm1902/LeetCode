cdc9394e242563329b6d4a55d67eb01261700762
Revanth Modalavalasa <revanthm051@gmail.com>
2026-05-20 01:42:25 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- counting/majority-element.py
+++ counting/majority-element.py
@@ -1,7 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n=len(nums)
+        res=count=0
         for num in nums:
-            count=sum(1 for i in nums if i==num)
-            if count>n//2:
-                return num
+            if count==0:
+                res=num
+            count += (1 if num==res else -1)
+        return res
