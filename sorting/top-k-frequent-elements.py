d2568b9f48e52aa08775786ab6530653468ed93a
Revanth Modalavalasa <revanthm051@gmail.com>
2026-05-25 17:57:11 +0530

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

--- sorting/top-k-frequent-elements.py
+++ sorting/top-k-frequent-elements.py
@@ -1,15 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count={}
-        for num in nums:
-            count[num]=count.get(num,0)+1
-        freq=[ [] for i in range(len(nums)+1) ]
-
-        for num,cnt in count.items():
-            freq[cnt].append(num)
-        res=[]
-        for i in range(len(freq)-1,0,-1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res)==k:
-                    return res
+        cnt=Counter(nums)
+        heap=[]
+        for num,freq in cnt.items():
+            heapq.heappush(heap,(freq,num))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        return [num for freq,num in heap]
