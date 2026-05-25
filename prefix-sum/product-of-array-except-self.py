0a58d4400c1b07bae4aeab0090d018063dd2fd83
Revanth Modalavalasa <revanthm051@gmail.com>
2026-05-25 21:40:40 +0530

    LC-product-of-array-except-self [Runtime: N/A; Memory: N/A]

--- prefix-sum/product-of-array-except-self.py
+++ prefix-sum/product-of-array-except-self.py
@@ -1,13 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        n = len(nums)
 
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+        left_products = [1] * n
+        right_products = [1] * n
+        res = [1] * n
+
+        for i in range(1, n):
+            left_products[i] = left_products[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            right_products[i] = right_products[i + 1] * nums[i + 1]
+            
+        for i in range(n):
+            res[i] = left_products[i] * right_products[i]
+            
         return res
