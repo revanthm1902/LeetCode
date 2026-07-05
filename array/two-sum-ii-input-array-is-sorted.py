7fc37cd64fa245056d7107ce18a6861a576cb7e0
Revanth Modalavalasa <revanthm051@gmail.com>
2026-07-05 10:57:08 +0530

    LC-two-sum-ii-input-array-is-sorted [Runtime: N/A; Memory: N/A]

--- array/two-sum-ii-input-array-is-sorted.py
+++ array/two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,18 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        mp={}
-        for i,num in enumerate(numbers):
-            rem=target-num
-            if rem in mp:
-                return [mp[rem]+1,i+1]
-            mp[num]=i
+        # mp={}
+        # for i,num in enumerate(numbers):
+        #     rem=target-num
+        #     if rem in mp:
+        #         return [mp[rem]+1,i+1]
+        #     mp[num]=i
+        l,r=0,len(numbers)-1
+        while l<r:
+            cursum=numbers[l]+numbers[r]
+            if cursum<target:
+                l+=1
+            elif cursum>target:
+                r-=1
+            else:
+                return [l+1,r+1]
+        return []
