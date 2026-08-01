54f44604c274b6340254def8d5f6aab71b6b89fc
Revanth Modalavalasa <revanthm051@gmail.com>
2026-08-02 05:04:25 +0530

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- monotonic-queue/sliding-window-maximum.py
+++ monotonic-queue/sliding-window-maximum.py
@@ -5,22 +5,17 @@ class Solution:
         #     res.append(max(nums[i:i+k]))
         # return res
 
-        q = deque()  # Stores indices of elements in monotonically decreasing order
-        res = []
-        
+        q=deque()
+        res=[]
+        l,r=0,0
         for r in range(len(nums)):
-            # 1. Pop smaller elements from the back (maintaining decreasing order)
-            while q and nums[q[-1]] < nums[r]:
+            while q and nums[q[-1]]<nums[r]:
                 q.pop()
-                
             q.append(r)
-            
-            # 2. Remove left out-of-bound index from the front
-            if q[0] < r - k + 1:
+            if l>q[0]:
                 q.popleft()
-                
-            # 3. Append maximum to result once the first window of size k is formed
-            if r >= k - 1:
+            if r>=k-1:
                 res.append(nums[q[0]])
-                
+                l+=1
+            r+=1
         return res
