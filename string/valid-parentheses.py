2f44f59dddbb818db55fcaeded597318fc4fe51f
Revanth Modalavalasa <revanthm051@gmail.com>
2026-08-02 05:55:47 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -1,7 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        while "()" in s or "{}" in s or "[]" in s:
-            s=s.replace("()","")
-            s=s.replace("{}","")
-            s=s.replace("[]","")
-        return s==""
+        # while "()" in s or "{}" in s or "[]" in s:
+        #     s=s.replace("()","")
+        #     s=s.replace("{}","")
+        #     s=s.replace("[]","")
+        # return s==""
+
+        st=[]
+        closeToOpen={")":"(","}":"{","]":"["}
+
+        for c in s:
+            if c in closeToOpen:
+                if st and st[-1]==closeToOpen[c]:
+                    st.pop()
+                else:
+                    return False
+            else:
+                st.append(c)
+        return True if not st else False
