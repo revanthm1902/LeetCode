d8fd9585c970243a1d95e4aa2ede5340b5474491
Revanth Modalavalasa <revanthm051@gmail.com>
2026-08-14 00:16:23 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

--- simulation/backspace-string-compare.py
+++ simulation/backspace-string-compare.py
@@ -1,12 +1,14 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def build(x):
-            st=[]
-            for ch in x:
-                if ch!="#":
-                    st.append(ch)
-                else:
-                    if st:
-                        st.pop()
-            return "".join(st)
-        return build(s)==build(t)
+        
+        def build(s):
+            stack = []
+            for c in s:
+                if c != '#':
+                    stack.append(c)
+                elif stack:
+                    stack.pop()
+            
+            return "".join(stack)
+        
+        return build(s) == build(t)
